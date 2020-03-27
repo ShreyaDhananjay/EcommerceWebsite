f7404936b21dd90f7a7d961dc2d9b28296e602c9
Shreya <shreyadj5@gmail.com>
2020-03-27 13:34:53 +0530

    flask moment for proper datetime

--- ecommerceweb/__init__.py
+++ ecommerceweb/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ka246'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site1.db'
+moment = Moment(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
